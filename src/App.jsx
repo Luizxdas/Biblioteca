7153276a13f23a811e37b7c6be038d91a5d6cec9
Luiz Augusto <luizxdas@outlook.com>
2024-10-24 15:40:29 -0300

    Adiciona e implementa o React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import Books from "./components/Books";
 import Title from "./components/Title";
 import SearchBar from "./components/SearchBar";
@@ -14,34 +15,41 @@ function App() {
   const [booksOrder, setBooksOrder] = useState("");
 
   return (
-    <div>
-      <div className="flex m-3 justify-end">
-        <div className="p-1">
-          <Register />
-        </div>
-        <div className="p-1">
-          <Login />
-        </div>
-      </div>
-      <div className="flex flex-col items-center mt-8">
-        <Title />
-        <SearchBar search={search} setSearch={setSearch} />
-        <div className="mt-1 mb-5">
-          <FilterButton
-            genres={genres}
-            selectedGenres={selectedGenres}
-            setSelectedGenres={setSelectedGenres}
-          />
-          <OrderButton setBooksOrder={setBooksOrder} />
-        </div>
-        <Books
-          search={search}
-          setGenres={setGenres}
-          selectedGenres={selectedGenres}
-          booksOrder={booksOrder}
-        />
-      </div>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div>
+            <div className="flex m-3 justify-end">
+              <div className="p-1">
+                <Register />
+              </div>
+              <div className="p-1">
+                <Login />
+              </div>
+            </div>
+            <div className="flex flex-col items-center mt-8">
+              <Title />
+              <SearchBar search={search} setSearch={setSearch} />
+              <div className="mt-1 mb-5">
+                <FilterButton
+                  genres={genres}
+                  selectedGenres={selectedGenres}
+                  setSelectedGenres={setSelectedGenres}
+                />
+                <OrderButton setBooksOrder={setBooksOrder} />
+              </div>
+              <Books
+                search={search}
+                setGenres={setGenres}
+                selectedGenres={selectedGenres}
+                booksOrder={booksOrder}
+              />
+            </div>
+          </div>
+        }
+      />
+    </Routes>
   );
 }
 
