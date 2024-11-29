66a002d13d90a968a8d93c7aad7e8695e6347f58
Luiz Augusto <luizxdas@outlook.com>
2024-11-29 10:40:52 -0300

    Remove comentários e interceptor inúteis, e move geração de auth_token para apiService.js

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { useAuth } from "./auth/useAuth";
 
 axios.defaults.withCredentials = true;
 
@@ -7,9 +6,24 @@ export const api = axios.create({
   baseURL: "https://localhost:8081",
 });
 
-{
-  /*  Conecta a API, salva os dados (setBooks) e os gêneros (setGenres). */
-}
+export const refreshToken = async () => {
+  try {
+    const response = await api.post("/auth/refresh");
+    if (response.status === 200) {
+      return true;
+    } else if (response.status === 400) {
+      throw new Error(
+        "Falha ao obter refresh token: refresh_token não encontrado."
+      );
+    } else {
+      throw new Error("Erro desconhecido ao obter refresh token");
+    }
+  } catch (error) {
+    console.error("Erro no refreshToken:", error);
+    throw error;
+  }
+};
+
 export const fetchBooks = async (setBooks, setLoading, setGenres) => {
   try {
     const response = await api.get("/books");
@@ -19,34 +33,12 @@ export const fetchBooks = async (setBooks, setLoading, setGenres) => {
     const uniqueGenres = [...new Set(response.data.map((book) => book.genre))];
     setGenres(uniqueGenres);
   } catch (error) {
+    if (error.response && error.response.status === 401) {
+      return;
+    }
     console.error(error);
     setLoading(false);
   }
 };
-
-{
-  /*  Intercepta e trata o erro 401. */
-}
-api.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    if (error.response && error.response.status === 401) {
-      try {
-        const { checkLoginStatus } = useAuth();
-        await checkLoginStatus();
-
-        return axios();
-      } catch (refreshError) {
-        console.log("Refresh token inválido, deslogando...");
-        window.location.href = "/?login=true"; // Ou use React Router: history.push('/login');
-        return Promise.reject(refreshError);
-      }
-    }
-
-    return Promise.reject(error);
-  }
-);
 
 export default api;
