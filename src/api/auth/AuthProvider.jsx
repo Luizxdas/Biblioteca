305d99db218a1c50d71b774b0fa2e4e5d4613fd4
Luiz Augusto <luizxdas@outlook.com>
2024-11-29 10:41:33 -0300

    Move função checkLoginStatus para o topo e remove adição de username ao localStorage

--- src/api/auth/AuthProvider.jsx
+++ src/api/auth/AuthProvider.jsx
@@ -1,72 +1,36 @@
 import PropTypes from "prop-types";
 import AuthContext from "./AuthContext";
 import { useEffect, useState } from "react";
-import { api } from "../apiService";
+import { api, refreshToken } from "../apiService";
 
 export const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    const username = localStorage.getItem("username");
-    if (username) {
-      setIsLoggedIn(true);
-    }
+    const checkLoginStatus = async () => {
+      const refresh = await refreshToken();
+
+      if (refresh) {
+        setIsLoggedIn(true);
+      }
+    };
+
+    checkLoginStatus();
   }, []);
-
-  const register = async (email, username, password) => {
-    if (!email || !username || !password) {
-      return { error: "Todos os campos são obrigatórios." };
-    }
-
-    try {
-      const response = await api.post(
-        "/auth/register",
-        { email, username, password },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      return {
-        success: true,
-        data: response.data,
-      };
-    } catch (error) {
-      const status = error.response ? error.response.status : null;
-      const errorMessage = error.response
-        ? error.response.data
-        : "Erro desconhecido ao registrar";
-
-      console.error("Erro ao fazer registro:", errorMessage);
-      return {
-        success: false,
-        error: errorMessage,
-        statusCode: status,
-      };
-    }
-  };
 
   const login = async (username, password) => {
     if (!username || !password) {
       return { error: "Todos os campos são obrigatórios." };
     }
+
     try {
       const response = await api.post(
         "/auth/login",
-        {
-          username,
-          password,
-        },
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
+        { username, password },
+        { headers: { "Content-Type": "application/json" } }
       );
 
       setIsLoggedIn(true);
-      localStorage.setItem("username", username);
 
       return {
         success: true,
@@ -91,7 +55,6 @@ export const AuthProvider = ({ children }) => {
   const logout = async () => {
     try {
       await api.post("/auth/logout");
-      localStorage.removeItem("username");
       setIsLoggedIn(false);
     } catch (error) {
       const status = error.response ? error.response.status : null;
@@ -108,19 +71,14 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-  const checkLoginStatus = async () => {
-    try {
-      const response = await api.post("/auth/refresh");
-      setIsLoggedIn(response.data.isLoggedIn);
-    } catch (error) {
-      console.error("Erro ao verificar status de login:", error);
-      setIsLoggedIn(false);
-    }
-  };
-
   return (
     <AuthContext.Provider
-      value={{ register, checkLoginStatus, login, logout, isLoggedIn }}
+      value={{
+        login,
+        logout,
+        isLoggedIn,
+        refreshToken,
+      }}
     >
       {children}
     </AuthContext.Provider>
