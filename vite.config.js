beb1be3014c4523547875f6f5b3aaa793fb0d3e7
Luiz Augusto <luizxdas@outlook.com>
2024-11-29 19:49:27 -0300

    Habilita o https para desenvolvimento local

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https:
+      // eslint-disable-next-line no-undef
+      process.env.NODE_ENV === "development"
+        ? {
+            key: fs.readFileSync("./certs/localhost.key"),
+            cert: fs.readFileSync("./certs/localhost.crt"),
+          }
+        : false,
+    host: "localhost",
+    port: 5173,
+  },
+});
