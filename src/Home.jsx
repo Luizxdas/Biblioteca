1b996cf6862c0380fe7332d1670a1671b7097acc
Luiz Augusto <luizxdas@outlook.com>
2024-12-03 14:16:06 -0300

    Remove funcionalidades de login e registro para mover para outro projeto

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,39 +1,18 @@
 import { useState } from "react";
-import { useAuth } from "./api/auth/useAuth";
 import Books from "./components/Books";
 import Title from "./components/Title";
 import SearchBar from "./components/SearchBar";
 import FilterButton from "./components/FilterButton";
 import OrderButton from "./components/OrderButton";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import Username from "./components/Username";
 
 function Home() {
   const [search, setSearch] = useState("");
   const [genres, setGenres] = useState("");
   const [selectedGenres, setSelectedGenres] = useState([]);
   const [booksOrder, setBooksOrder] = useState("");
-  const { isLoggedIn } = useAuth();
 
   return (
     <div>
-      {!isLoggedIn && (
-        <div className="flex justify-end m-3">
-          <div className="p-1">
-            <Register />
-          </div>
-          <div className="p-1">
-            <Login />
-          </div>
-        </div>
-      )}
-      {isLoggedIn && (
-        <div className="flex justify-end m-3 mt-6 mr-16">
-          <Username />
-        </div>
-      )}
-      {}
       <div className="flex flex-col items-center mt-8">
         <Title />
         <SearchBar search={search} setSearch={setSearch} />
--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,48 +1,8 @@
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
-
 export const api = axios.create({
-  baseURL: "https://localhost:8081",
+  baseURL: "http://localhost:8081",
 });
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response && error.response.status === 401) {
-      console.warn(
-        "Não autorizado: Usuário não autenticado ou sessão expirada."
-      );
-      return Promise.resolve(null);
-    } else if (error.response && error.response.status === 400) {
-      console.warn(
-        "Não autenticado: Usuário não autenticado ou token não encontrado."
-      );
-      return Promise.resolve(null);
-    } else {
-      console.error("Erro inesperado:", error);
-      return Promise.reject(error);
-    }
-  }
-);
-
-export const refreshToken = async () => {
-  const response = await api.post("/auth/refresh");
-  return response ? true : false;
-};
-
-export const fetchUserData = async () => {
-  try {
-    const response = await api.get("/user");
-    return response.data;
-  } catch (error) {
-    if (error.response && error.response.status === 401) {
-      return null;
-    }
-    console.error(error);
-    throw error;
-  }
-};
 
 export const fetchBooks = async () => {
   try {
@@ -52,6 +12,18 @@ export const fetchBooks = async () => {
     const uniqueGenres = [...new Set(books.map((book) => book.genre))];
 
     return { books, genres: uniqueGenres };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const fetchBook = async (id) => {
+  try {
+    const response = await api.get(`/books/${id}`);
+    const book = response.data;
+
+    return book;
   } catch (error) {
     if (error.response && error.response.status === 401) {
       return null;
