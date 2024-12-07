7921a24151e8b3b9858fc62e02b53f5f606dff69
Luiz Augusto <luizxdas@outlook.com>
2024-12-07 15:22:14 -0300

    Remove comentários inúteis e adiciona função para checar se string é um URL válido

--- src/components/utils/utils.js
+++ src/components/utils/utils.js
@@ -1,8 +1,5 @@
 import { useEffect } from "react";
 
-{
-  /* Detecta clique fora da div e fecha ela. */
-}
 export function useClickOutsideToClose(ref, setIsOpen) {
   useEffect(() => {
     function handleClickOutside(event) {
@@ -16,3 +13,19 @@ export function useClickOutsideToClose(ref, setIsOpen) {
     };
   }, [ref, setIsOpen]);
 }
+
+export const isValidURL = (string) => {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?([a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?)$",
+    "i"
+  );
+  return pattern.test(string);
+};
+
+export const creditsStyle = {
+  linkStyle:
+    "text-lg text-center text-blue-600 underline hover:text-blue-800 bg-gray-300",
+  creditsDiv: "flex flex-col bg-gray-400",
+  creditsTitle: "mb-2 text-xl text-center",
+  creditsSpan: "block text-lg text-center bg-gray-300",
+};
