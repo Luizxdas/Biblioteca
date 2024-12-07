f303a36667ae1b574762ca64bce604fd8af0226a
Luiz Augusto <luizxdas@outlook.com>
2024-12-07 15:28:33 -0300

    Remove comentários inúteis e simplifica função de fechar modal ao clicar fora

--- src/components/OrderButton.jsx
+++ src/components/OrderButton.jsx
@@ -1,46 +1,31 @@
 import PropTypes from "prop-types";
-import { useState, useEffect } from "react";
+import { useState, useRef } from "react";
+import { useClickOutsideToClose } from "./utils/utils";
 
 function OrderButton({ setBooksOrder }) {
   const [isOpen, setIsOpen] = useState(false);
+  const dropdownRef = useRef(null);
 
-  {
-    /* Fecha/abre as opções. */
-  }
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
+
+  useClickOutsideToClose(dropdownRef, setIsOpen);
 
   const handleOrderChange = (event) => {
     setBooksOrder(event.target.name);
   };
 
-  {
-    /* Clicar fora da div de opções fecha ela. */
-  }
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (!event.target.closest(".dropOrder")) {
-        setIsOpen(false);
-      }
-    };
-
-    document.addEventListener("click", handleClickOutside);
-    return () => {
-      document.removeEventListener("click", handleClickOutside);
-    };
-  }, []);
-
   return (
-    <div className="relative inline-block dropOrder m-2">
+    <div className="relative inline-block m-2 dropOrder">
       <button
         onClick={toggleDropdown}
-        className="bg-stone-300 text-gray-700 px-4 py-2 rounded-md focus:outline-none"
+        className="px-4 py-2 text-gray-700 rounded-md shadow-md bg-slate-200 focus:outline-none"
       >
         Ordenar
       </button>
       {isOpen && (
-        <div className="absolute mt-2 left-1/2 transform -translate-x-1/2 w-48 bg-stone-300 shadow-md rounded-md p-2">
+        <div className="absolute w-48 p-2 mt-2 transform -translate-x-1/2 rounded-md shadow-md left-1/2 bg-slate-200">
           <div className="flex flex-col">
             <button name="name" onClick={handleOrderChange}>
               Nome
