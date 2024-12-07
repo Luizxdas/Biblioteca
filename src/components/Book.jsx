f29ad5317d4cb565e77462f89443f1d97044a5f0
Luiz Augusto <luizxdas@outlook.com>
2024-12-07 15:44:13 -0300

    Centraliza loading e título; remove bordas de debug; adapta layout para dispositivos móveis; trata ausência de imagem ou livro

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -3,6 +3,7 @@ import { fetchBook } from "../api/apiService";
 import { useNavigate, useParams } from "react-router-dom";
 import { ChevronLeft } from "lucide-react";
 import Title from "./Title";
+import Credits from "./Credits";
 
 function Book() {
   const { id } = useParams();
@@ -24,20 +25,20 @@ function Book() {
   };
 
   if (loading) {
-    return <div>Carregando...</div>;
-  }
-
-  if (!book) {
-    return <div>Livro não encontrado</div>;
+    return (
+      <div className="h-screen">
+        <div className="flex items-center justify-center">Carregando...</div>
+      </div>
+    );
   }
 
   return (
     <div>
-      <div className="mt-10">
+      <div className="flex flex-col items-center mt-10">
         <Title />
       </div>
       <div className="flex justify-center h-screen mt-10">
-        <div className="border border-blue-500 w-[80em] h-[38em] flex justify-between">
+        <div className="shadow-xl bg-slate-200 w-[20em] h-fit md:w-[80em] md:h-[38em] md:flex justify-between">
           <div className="absolute mt-4">
             <button
               onClick={handleGoHome}
@@ -46,18 +47,39 @@ function Book() {
               <ChevronLeft size={38} />
             </button>
           </div>
-          <div className="mt-5 ml-36 w-fit">
-            <img
-              src={book.image}
-              alt="Imagem de capa do livro"
-              className="w-44 h-72 md:w-[24em] md:h-[35em]"
-            />
+          <div className="mt-5 ml-16 md:ml-36 w-fit">
+            {book !== null ? (
+              <img
+                src={book.image}
+                alt="Imagem de capa do livro"
+                className="w-48 h-64 md:w-[24em] md:h-[35em] shadow-lg rounded-lg"
+              />
+            ) : (
+              <div className="w-44 h-72 md:w-[24em] md:h-[35em] bg-gray-300 flex items-center justify-center">
+                <span className="text-gray-500">Sem imagem disponível</span>
+              </div>
+            )}
           </div>
 
-          <div className="flex flex-col items-center mt-4 mr-44 w-[30em]">
-            <h1 className="text-4xl font-medium">{book.title}</h1>
-            <h2 className="text-2xl font-medium text-center">{book.author}</h2>
-            <p className="mt-4 text-xl">{book.synopsis}</p>
+          <div className="flex flex-col items-center mt-4 mr-44 w-full p-4 md:w-[30em] md:p-0">
+            {book !== null ? (
+              <>
+                <h1 className="text-4xl font-medium">{book.title}</h1>
+                <h2 className="text-2xl font-medium text-center">
+                  {book.author}
+                </h2>
+                <p className="mt-4 overflow-scroll text-xl h-[18em] md:h-[21em] md:mb-0 mb-8">
+                  {book.synopsis}
+                </p>
+                <div className="mt-auto">
+                  <Credits />
+                </div>
+              </>
+            ) : (
+              <h1 className="text-2xl font-medium text-center">
+                Livro não encontrado
+              </h1>
+            )}
           </div>
         </div>
       </div>
